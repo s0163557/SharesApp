76f2af93cb858aa4b77709fa83c7532429a4526e
Konstantin Lishenkov <kostya.lishenkov.03@mail.ru>
2025-04-16 17:52:01 +0300

    add deletion to week gathering

--- SharesApp.Server/Controllers/MoexController.cs
+++ SharesApp.Server/Controllers/MoexController.cs
@@ -333,7 +333,17 @@ namespace Stock_Analysis_Web_App.Controllers
                             newListOfWeekTrades.Add(securityTradeRecord);
                         }
                     }
-                    dbContext.SecurityTradeRecordsByWeek.AddRange(newListOfWeekTrades);
+                    foreach (var record in newListOfWeekTrades)
+                    {
+                        var existingItem = dbContext.SecurityTradeRecordsByWeek.FirstOrDefault(x => x.DateOfTrade == record.DateOfTrade &&
+                                                                                            x.SecurityInfo == record.SecurityInfo);
+                        //Если нашли старые совпадающие записи, то удалим, и накатнем поверх новые.
+                        if (existingItem != null)
+                            dbContext.SecurityTradeRecordsByWeek.Remove(existingItem);
+
+                        dbContext.SecurityTradeRecordsByWeek.Add(record);
+                    }
+
                     dbContext.SaveChanges();
                 }
             }
